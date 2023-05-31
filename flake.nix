254e9206371f6c6b72084791bb734dce8e173bc3
nils <48135649+Nilstrieb@users.noreply.github.com>
2023-05-31 08:36:40 +0200

    flake

--- flake.nix
+++ flake.nix
@@ -1,15 +1,12 @@
 {
-  description = "Example JavaScript development environment for Zero to Nix";
+  description = "Kurs React and Spring Boot Vertiefung ITA 2023";
 
-  # Flake inputs
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs"; # also valid: "nixpkgs"
+    nixpkgs.url = "github:NixOS/nixpkgs";
   };
 
-  # Flake outputs
   outputs = { self, nixpkgs }:
     let
-      # Systems supported
       allSystems = [
         "x86_64-linux" # 64-bit Intel/AMD Linux
         "aarch64-linux" # 64-bit ARM Linux
@@ -17,18 +14,17 @@
         "aarch64-darwin" # 64-bit ARM macOS
       ];
 
-      # Helper to provide system-specific attributes
       forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
         pkgs = import nixpkgs { inherit system; };
       });
     in
     {
-      # Development environment output
       devShells = forAllSystems ({ pkgs }: {
         default = pkgs.mkShell {
-          # The Nix packages provided in the environment
           packages = with pkgs; [
             nodejs-18_x # Node.js 18, plus npm, npx, and corepack
+            gradle
+            jdk18
           ];
         };
       });
