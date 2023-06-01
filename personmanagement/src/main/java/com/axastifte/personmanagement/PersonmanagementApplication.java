d91b1afddee36d3d406e768f776398ceb351866c
nils <48135649+Nilstrieb@users.noreply.github.com>
2023-06-01 13:22:18 +0200

    personsmfndfme

---
+++ personmanagement/src/main/java/com/axastifte/personmanagement/PersonmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.axastifte.personmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonmanagementApplication.class, args);
+    }
+
+}
